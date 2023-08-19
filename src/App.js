a558b3184aeeebcf33d0960381c312486bd625f1
akrazalive <akrazalive@gmail.com>
2023-08-19 10:52:21 +0500

    Added some request url to get data from

--- src/App.js
+++ src/App.js
@@ -9,13 +9,25 @@ const dummyProduct = {
     image: 'https://pakistani.pk/uploads/reviews/photos/original/4b/1e/20//Hande20ErC3A7el203-30-1507621486.jpg',
 };
 
+
 function App() {
     const [barcode, setBarcode] = useState('');
     const [product, setProduct] = useState(null);
 
-    const handleEnter = (e) => {
+    const handleEnter = async (e) => {
         if (e.key === 'Enter') {
-            setProduct(dummyProduct);
+            try {
+                const response = await fetch(`https://rapidfiller.com/wp-json/barcode/v1/get-product/?barcode=${barcode}`);
+                const data = await response.json();
+
+                if (response.ok) {
+                    setProduct(data);
+                } else {
+                    console.error('API error:', data.message);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         }
     };
 
@@ -32,10 +44,12 @@ function App() {
             </div>
             {product && (
                 <div className="product-details">
-                    <h2>{product.name}</h2>
-                    <p>Price: {product.price}</p>
-                    <div className="desc">{product.description}</div>
-                    <img src={product.image} alt={product.name} />
+                    <h2>{product.title}</h2>
+                    <p>Regular Price: {product.regular_price}</p>
+                    <p>Sale Price: {product.sale_price}</p>
+                    <p>Stock Status: {product.stock_status}</p>
+                    <p>Stock Quantity: {product.stock_quantity}</p>
+                    <img src={product.thumbnail_url} alt={product.title} />
                 </div>
             )}
         </div>
